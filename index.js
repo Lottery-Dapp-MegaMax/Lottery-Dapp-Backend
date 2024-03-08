89c8fd0686562fa0b9cdb5b51243452727010935
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-03-08 14:39:37 +0700

    updated server

--- index.js
+++ index.js
@@ -1,64 +1,141 @@
 import { ethers } from "ethers";
 import dotenv from "dotenv";
 import vault from "./Vault.json" assert { type: "json" };
+import pool from "./Pool.json" assert { type: "json" };
+import token from "./Token.json" assert { type: "json" };
 import express from "express";
-const app = express();
-const port = 3000;
-
+import cron from "node-cron";
+import bodyParser from "body-parser";
+import cors from "cors";
+import Queue from "bull";
 dotenv.config();
 
-let confluxTestnetProvider,
-	mumbaiPolygonProvider,
-	mumbaiPolygonWallet,
-	confluxTestnetWallet;
-async function init() {
-	mumbaiPolygonProvider = await new ethers.providers.JsonRpcProvider(
-		"https://rpc-mumbai.maticvigil.com/"
-	);
-	mumbaiPolygonWallet = await new ethers.Wallet(
-		process.env.WALLET_PRIVATE_KEY,
-		mumbaiPolygonProvider
-	);
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-	confluxTestnetProvider = await new ethers.providers.JsonRpcProvider(
-		"http://127.0.0.1:8545"
-	);
-	confluxTestnetWallet = await new ethers.Wallet(
-		process.env.WALLET_PRIVATE_KEY,
-		confluxTestnetProvider
+app.get("/status", (request, response) =>
+	response.json({ clients: clients.length })
+);
+
+const port = 3000;
+const USDT_Address_Mumbai = "0x1fde0ecc619726f4cd597887c9f3b4c8740e19e2";
+const sendTxQueue = new Queue("send tx queue");
+// provider and signer
+
+const mumbaiPolygonProvider = new ethers.providers.JsonRpcProvider(
+	"https://rpc-mumbai.maticvigil.com/"
+);
+
+const mumbaiPolygonWallet = new ethers.Wallet(
+	process.env.WALLET_PRIVATE_KEY,
+	mumbaiPolygonProvider
+);
+
+const confluxTestnetProvider = new ethers.providers.JsonRpcProvider(
+	"https://evmtestnet.confluxrpc.com"
+);
+
+const confluxTestnetWallet = new ethers.Wallet(
+	process.env.WALLET_PRIVATE_KEY,
+	confluxTestnetProvider
+);
+
+const webSocketProvider = new ethers.providers.WebSocketProvider(
+	"wss://evmtestnet.confluxrpc.com/ws"
+);
+
+// contract
+
+const contractVaultInConflux = new ethers.Contract(
+	"0x98d9945439d9736ce5120ace449363f22b413943",
+	vault,
+	webSocketProvider
+);
+
+const aavePoolProxyContractInMumbai = new ethers.Contract(
+	"0xcc6114b983e4ed2737e9bd3961c9924e6216c704",
+	pool,
+	mumbaiPolygonWallet
+);
+
+const USDT_Token_Mumbai = new ethers.Contract(
+	USDT_Address_Mumbai,
+	token,
+	mumbaiPolygonWallet
+);
+
+USDT_Token_Mumbai.approve(
+	aavePoolProxyContractInMumbai.address,
+	Number(await USDT_Token_Mumbai.balanceOf(mumbaiPolygonWallet.address))
+);
+
+contractVaultInConflux.on("_Deposit", async (poolAddress, receiver, assets) => {
+	console.log("First parameter: ", poolAddress);
+	console.log("Third parameter: ", receiver);
+	console.log("Four parameter: ", Number(assets));
+
+	await sendTxQueue.add({ poolAddress, receiver, assets });
+});
+
+// Array to hold connections
+let clients = [];
+let txs = [];
+
+function eventsHandler(request, response, next) {
+	console.log("have connection");
+	const headers = {
+		"Content-Type": "text/event-stream",
+		Connection: "keep-alive",
+		"Cache-Control": "no-cache",
+	};
+	response.writeHead(200, headers);
+
+	const data = `data: ${JSON.stringify(txs)}\n\n`;
+
+	response.write(data);
+
+	const clientId = Date.now();
+
+	const newClient = {
+		id: clientId,
+		response,
+	};
+
+	clients.push(newClient);
+
+	request.on("close", () => {
+		console.log(`${clientId} Connection closed`);
+		clients = clients.filter((client) => client.id !== clientId);
+	});
+}
+
+app.get("/events", eventsHandler);
+
+function sendEventsToAll(newTxs) {
+	clients.forEach((client) =>
+		client.response.write(`data: ${JSON.stringify(newTxs)}\n\n`)
 	);
 }
 
-let signer = confluxTestnetWallet;
-const abi = vault.abi;
-
-const contractVaultInConflux = new ethers.Contract(
-	"0x775d85ff6d3aa949397e61de9c3a06f9d6efea47",
-	abi,
-	signer
-);
+sendTxQueue.process(async (job, done) => {
+	const success = await aavePoolProxyContractInMumbai.supply(
+		USDT_Address_Mumbai,
+		job.data.assets,
+		job.data.receiver,
+		0
+	);
+	txs.push(success.hash);
+	sendEventsToAll(success.hash);
+	console.log("done\n");
+	done();
+});
 
 app.get("/", (req, res) => {
 	res.send(contractVaultInConflux.address);
-	contractVaultInConflux.on(
-		"Deposit",
-		(poolAddress, sender, receiver, assets, shares) => {
-			console.log(
-				` ${sender} call contract Vault to deposit to ${poolAddress} with amount assets of ${assets} and ${receiver} get amount shares of ${shares}`
-			);
-		}
-	);
-	contractVaultInConflux.on(
-		"Withdraw",
-		(poolAddress, sender, receiver, owner, assets, shares) => {
-			console.log(
-				` ${sender} call contract Vault to withdraw from ${owner} at ${poolAddress} with amount assets of ${assets} and ${receiver} get amount shares of ${shares}`
-			);
-		}
-	);
 });
 
 app.listen(port, async () => {
-	await init();
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Txs Events service listening at http://localhost:${port}`);
 });
